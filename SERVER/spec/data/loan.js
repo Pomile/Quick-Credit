521c4fac5b0119a5e54fb28480f5e30b7f37634c
Babatunde <soft-sky@live.co.uk>
2019-05-04 15:45:41 +0100

    chore(loan-application-test):add test for loan application
    -Create loan api test
    -Modify package.json
    
    [Finishes #165807124]

--- SERVER/spec/data/loan.js
+++ SERVER/spec/data/loan.js
@@ -1,7 +1,17 @@
 const loan = {
-    
-    credit: {
-        amount: 20000,
-        tenor: 5,
-    }
-}
+
+  user1creditRequest: {
+    amount: 200000,
+    tenor: 5,
+  },
+  user1creditRequestWithoutAmount: {
+    amount: 0,
+    tenor: 5,
+  },
+  user1creditRequestWithoutTenor: {
+    amount: 200000,
+    tenor: 0,
+  },
+};
+
+export default loan;
---
+++ SERVER/spec/loan.js
@@ -0,0 +1,65 @@
+import chai from 'chai';
+import request from 'supertest';
+import app from '../server';
+import userData from './data/user';
+import loanData from './data/loan';
+
+const { expect } = chai;
+
+describe('QUICK-CREDIT Test Suite', () => {
+  describe('Loan API', () => {
+    it('A user should be able to apply for a loan', (done) => {
+      const { token, isAuth } = userData.userAuth;
+      request(app)
+        .post('/api/v1/loans')
+        .set('Accept', 'application/json')
+        .set({ authorization: `${token}`, isAuth: `${isAuth}` })
+        .send(loanData.user1creditRequest)
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          expect(res.body.data.user).to.equal(1);
+          expect(res.body.data.amount).to.equal(200000);
+          done();
+        });
+    });
+    it('A user should not be able to apply for a loan if previous loan is not repaid', (done) => {
+      const { token, isAuth } = userData.userAuth;
+      request(app)
+        .post('/api/v1/loans')
+        .set('Accept', 'application/json')
+        .set({ authorization: `${token}`, isAuth: `${isAuth}` })
+        .send(loanData.user1creditRequest)
+        .end((err, res) => {
+          expect(res.status).to.equal(409);
+          expect(res.body.error).to.equal('Previous loan not repaid');
+          done();
+        });
+    });
+    it('A user should not be able to apply for a loan without amount', (done) => {
+      const { token, isAuth } = userData.userAuth;
+      request(app)
+        .post('/api/v1/loans')
+        .set('Accept', 'application/json')
+        .set({ authorization: `${token}`, isAuth: `${isAuth}` })
+        .send(loanData.user1creditRequestWithoutAmount)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.error).to.equal('Loan amount is required');
+          done();
+        });
+    });
+    it('A user should not be able to apply for a loan without tenor', (done) => {
+      const { token, isAuth } = userData.userAuth;
+      request(app)
+        .post('/api/v1/loans')
+        .set('Accept', 'application/json')
+        .set({ authorization: `${token}`, isAuth: `${isAuth}` })
+        .send(loanData.user1creditRequestWithoutTenor)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body.error).to.equal('Tenor is required');
+          done();
+        });
+    });
+  });
+});
