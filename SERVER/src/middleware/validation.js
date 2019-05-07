e313cd2c9eb2df3154af234f0f2d063e19a837ca
Babatunde <soft-sky@live.co.uk>
2019-05-08 00:52:54 +0100

    code maintainance review

--- SERVER/src/middleware/validation.js
+++ SERVER/src/middleware/validation.js
@@ -23,46 +23,46 @@ export const validateUser = (req, res, next) => {
 };
 
 export const validateCredentials = (req, res, next) => {
-  const fieldResult = fieldValidator(req, loginFields);
-  const userDataResult = userCredentialsValidator(req);
-  if (!fieldResult.allFieldExists) {
-    res.status(400).json({ error: fieldResult.error[0] }).end();
-  } else if (!userDataResult.isValid) {
-    res.status(422).json({ error: userDataResult.errors[0].error }).end();
+  const userCredFieldResult = fieldValidator(req, loginFields);
+  const userCredDataResult = userCredentialsValidator(req);
+  if (!userCredFieldResult.allFieldExists) {
+    res.status(400).json({ error: userCredFieldResult.error[0] }).end();
+  } else if (!userCredDataResult.isValid) {
+    res.status(422).json({ error: userCredDataResult.errors[0].error }).end();
   } else {
     next();
   }
 };
 
 export const validateHomeAddress = (req, res, next) => {
-  const fieldResult = fieldValidator(req, houseAddress);
-  const userDataResult = homeAddressValidator(req);
-  if (!fieldResult.allFieldExists) {
-    res.status(400).json({ error: fieldResult.error[0] }).end();
-  } else if (!userDataResult.isValid) {
-    res.status(422).json({ error: userDataResult.errors[0].error }).end();
+  const addressFieldResult = fieldValidator(req, houseAddress);
+  const userAddressDataResult = homeAddressValidator(req);
+  if (!addressFieldResult.allFieldExists) {
+    res.status(400).json({ error: addressFieldResult.error[0] }).end();
+  } else if (!userAddressDataResult.isValid) {
+    res.status(422).json({ error: userAddressDataResult.errors[0].error }).end();
   } else {
     next();
   }
 };
 
 export const validateJob = (req, res, next) => {
-  const fieldResult = fieldValidator(req, jobFields);
-  const userDataResult = userJObValidator(req);
-  if (!fieldResult.allFieldExists) {
-    res.status(400).json({ error: fieldResult.error[0] }).end();
-  } else if (!userDataResult.isValid) {
-    res.status(422).json({ error: userDataResult.errors[0].error }).end();
+  const jobFieldResult = fieldValidator(req, jobFields);
+  const userJobDataResult = userJObValidator(req);
+  if (!jobFieldResult.allFieldExists) {
+    res.status(400).json({ error: jobFieldResult.error[0] }).end();
+  } else if (!userJobDataResult.isValid) {
+    res.status(422).json({ error: userJobDataResult.errors[0].error }).end();
   } else {
     next();
   }
 };
 
 export const validateLoan = (req, res, next) => {
-  const fieldResult = fieldValidator(req, loanFields);
+  const loanFieldResult = fieldValidator(req, loanFields);
   const loanDataResult = loanValidator(req);
-  if (!fieldResult.allFieldExists) {
-    res.status(400).json({ error: fieldResult.error[0] }).end();
+  if (!loanFieldResult.allFieldExists) {
+    res.status(400).json({ error: loanFieldResult.error[0] }).end();
   } else if (!loanDataResult.isValid) {
     res.status(422).json({ error: loanDataResult.errors[0].error }).end();
   } else {
@@ -71,22 +71,22 @@ export const validateLoan = (req, res, next) => {
 };
 
 export const validateLoanStat = (req, res, next) => {
-  const fieldResult = fieldValidator(req, loanStatusFields);
-  const loanDataResult = validateLoanStatus(req);
-  if (!fieldResult.allFieldExists) {
-    res.status(400).json({ error: fieldResult.error[0] }).end();
-  } else if (!loanDataResult.isValid) {
-    res.status(422).json({ error: loanDataResult.errors[0].error }).end();
+  const loanStatFieldResult = fieldValidator(req, loanStatusFields);
+  const loanStatDataResult = validateLoanStatus(req);
+  if (!loanStatFieldResult.allFieldExists) {
+    res.status(400).json({ error: loanStatFieldResult.error[0] }).end();
+  } else if (!loanStatDataResult.isValid) {
+    res.status(422).json({ error: loanStatDataResult.errors[0].error }).end();
   } else {
     next();
   }
 };
 
 export const validateRepayment = (req, res, next) => {
-  const fieldResult = fieldValidator(req, repaymentFields);
+  const repayFieldResult = fieldValidator(req, repaymentFields);
   const repaymentDataResult = validateRepaymentVal(req);
-  if (!fieldResult.allFieldExists) {
-    res.status(400).json({ error: fieldResult.error[0] }).end();
+  if (!repayFieldResult.allFieldExists) {
+    res.status(400).json({ error: repayFieldResult.error[0] }).end();
   } else if (!repaymentDataResult.isValid) {
     res.status(422).json({ error: repaymentDataResult.errors[0].error }).end();
   } else {
