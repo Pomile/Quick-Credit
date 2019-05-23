a616d18fa0e583e72aea70fa0990d76a3e96a0ee
Babatunde <soft-sky@live.co.uk>
2019-05-23 13:18:40 +0100

    chore(frontend): improve frontend concepts, layout and design
    -Modify signin page
    -Modify signup page
    -Create footer
    -Modify user page
    -...
    [Finishes #166202885]

--- dist/src/models/address.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var address = "\nid serial NOT NULL,\nuserId int UNIQUE NOT NULL,\nhomeAddress character varying NOT NULL,\nstate character varying NOT NULL,\n\nCONSTRAINT \"Addresses_pkey\" PRIMARY KEY (id),\nCONSTRAINT \"Addresses_userId_fkey\" FOREIGN KEY (userId)\nREFERENCES Users (id)\nON UPDATE CASCADE\nON DELETE NO ACTION\n";
-var _default = address;
-exports["default"] = _default;
