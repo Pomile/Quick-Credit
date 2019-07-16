01317da0d4a2596b2438d2c70bbb741c88fead05
Babatunde <soft-sky@live.co.uk>
2019-07-16 07:59:27 +0100

    feat(recent-loan): implement frontend that display user recent loan
    - Create loan card template
    - Create a method that format date
    - Create a function that add a loan card
    - Modify user function that fetch data and display it in a dashboard

---
+++ CLIENT/actions/dashboard/template/loanCard.js
@@ -0,0 +1,29 @@
+const loanCardTemplate = data => `
+    <div class="loan">
+    <div class="loan__header -text-align-center -fs-res-l-1">Loan</div>
+    <div class="loan__display loan-shadow -text-align-center -fs-res-l-1">${data.id}</div>
+    <div class="row">
+            <div class="loan__label">Amount</div>
+            <div class="loan__output -text-align-center">${data.amount}</div>
+    </div>
+    <div class="row -border-top">
+        <div class="loan__label">Status</div>
+        <div class="loan__output -text-align-center">${data.status}</div>
+    </div>
+    <div class="row -border-top">
+            <div class="loan__label">Installment</div>
+            <div class="loan__output -text-align-center">${data.monthlyinstallment}</div>
+        </div>
+        <div class="row -border-top">
+                <div class="loan__label">Duration</div>
+                <div class="loan__output -text-align-center">${data.tenor} month(s)</div>
+            </div>
+    <div class="row -border-top">
+            <div class="loan__label">Due date</div>
+            <div class="loan__output -text-align-center">${data.duedate}</div>
+        </div>
+    
+</div>
+    `;
+
+export default loanCardTemplate;
--- CLIENT/actions/dashboard/user.js
+++ CLIENT/actions/dashboard/user.js
@@ -1,15 +1,26 @@
 import profileCard from './profileCard';
+import loanCard from './loanCard';
 
 const userDashboard = async () => {
   const token = localStorage.getItem('token');
   const userId = localStorage.getItem('id');
+  const email = localStorage.getItem('email');
   await fetch(`http://localhost:8000/api/v1/users/${userId}/profile`,
     {
       method: 'get',
       headers: { 'Content-Type': 'application/json', authorization: `${token}` },
     }).then(res => res.json()).then((data) => {
-    if (data.status) {
+    if (data.status === 200) {
       profileCard(data.data);
+    }
+  });
+  await fetch(`http://localhost:8000/api/v1/users/${email}/loan`,
+    {
+      method: 'get',
+      headers: { 'Content-Type': 'application/json', authorization: `${token}` },
+    }).then(res => res.json()).then((data) => {
+    if (data.status === 200) {
+      loanCard(data.data);
     }
   });
 };
