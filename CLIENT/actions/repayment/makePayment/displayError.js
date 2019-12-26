cc706a600ea27b749873aa0bfd8c3c359dfed6b9
Babatunde <soft-sky@live.co.uk>
2019-12-26 20:19:27 +0100

    bug[user-sign-up]: Fix user sign up
    - [x] Modify userDatavalidator in SERVER/src/middleware/validator
    - [x] Modify Package.json
    - [x] Modify spec
    - [x] Modify signin
    - [x] Modify signup
    - [x] Modify display error tool

--- CLIENT/actions/repayment/makePayment/displayError.js
+++ CLIENT/actions/repayment/makePayment/displayError.js
@@ -1,14 +1,24 @@
 
-const displayError = (error, isValid) => {
+const errorTemplate = (err) => {
+    const p1 = document.createElement('p');
+    const errMsg = document.querySelector('#errorMsg');
+    p1.className = '-text-align-center -fc-red';
+    p1.innerHTML = err;
+    errMsg.appendChild(p1);
+}
+const displayError = (errors, isValid) => {
   if (isValid) {
     document.querySelector('#msg').innerHTML = error;
     open('backdrop1', 'errorBox');
   } else {
-    const p1 = document.createElement('p');
-    const errMsg = document.querySelector('#errorMsg');
-    p1.className = '-text-align-center -fc-red';
-    p1.innerHTML = error;
-    errMsg.appendChild(p1);
+    if (Array.isArray(errors)){
+      for(counter=0; counter < errors.length; counter++ ){
+        errorTemplate(errors[counter].error)
+      }
+    } else{
+      errorTemplate(errors);
+    }
+    
   }
 };
 
--- CLIENT/actions/tools/displayError.js
+++ CLIENT/actions/tools/displayError.js
@@ -1,6 +1,22 @@
-const displayError = (msg) => {
-  document.getElementById('errorMsg').innerHTML = msg;
-  document.getElementById('errorMsg').style.color = 'red';
+import removeAllTableChildNode from './removeAllChildNode';
+
+const errorTemplate = (err) => {
+    const p1 = document.createElement('p');
+    const errMsg = document.querySelector('#errorMsg');
+
+    p1.className = '-text-align-center -fc-red';
+    p1.innerHTML = err;
+    errMsg.appendChild(p1);
+}
+const displayError = (errors) => {
+    errorMsg.innerHTML = '';
+    if (Array.isArray(errors)){
+      for(let counter=0; counter < errors.length; counter++ ){
+        errorTemplate(errors[counter].error)
+      }
+    } else{
+      errorTemplate(errors);
+    }
 };
 
 export default displayError;
