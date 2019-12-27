13ad89e7e52c20637e05d3bf0431a1cd98a1fd19
Babatunde <soft-sky@live.co.uk>
2019-12-27 01:27:20 +0100

    chore[modify-path-url]: Modify path url for production
    - [x] Modify page path
    - [x] Modify endpoint path
    
    [#]

--- CLIENT/route/endpointPath.js
+++ CLIENT/route/endpointPath.js
@@ -1,5 +1,5 @@
 
-const { NODE_ENV } = process.env;
-const baseUrl = NODE_ENV === 'https://quick-credit-v1.herokuapp.com/api/v1';
+const baseUrl = 'https://quick-credit-v1.herokuapp.com/api/v1';
+
 
 export default baseUrl;
--- CLIENT/route/pagePath.js
+++ CLIENT/route/pagePath.js
@@ -1,4 +1,5 @@
 // Quick-Credit/dist
+<<<<<<< Updated upstream
 const { NODE_ENV } = process.env;
 const home = NODE_ENV === 'development' ? '/index.html'
   : '/Quick-Credit/dist/index.html';
@@ -26,6 +27,21 @@ const repayment = NODE_ENV === 'development' ? '/repayment.html'
   : '/Quick-Credit/dist/repayment.html';
 const manageloan = NODE_ENV === 'development' ? '/manageloan.html'
   : '/Quick-Credit/dist/manageloan.html';
+=======
+const home = '/Quick-Credit/dist/index.html';
+const signup = '/Quick-Credit/dist/signup.html';
+const signin = '/Quick-Credit/dist/signin.html';
+const userDashboardPath = '/Quick-Credit/dist/user.html';
+const adminDashboardPath = '/Quick-Credit/dist/admin.html';
+const loanApplication = '/Quick-Credit/dist/loanapp.html';
+const profile = '/Quick-Credit/dist/profile.html';
+const passwordReset '/Quick-Credit/dist/reset.html';
+const verifyUser = '/Quick-Credit/dist/verifyuser.html';
+const loan = '/Quick-Credit/dist/manageloan.html';
+const makePayment = '/Quick-Credit/dist/makepayment.html';
+const repayment = '/Quick-Credit/dist/repayment.html';
+const manageloan = '/Quick-Credit/dist/manageloan.html';
+>>>>>>> Stashed changes
 
 export {
   home,
